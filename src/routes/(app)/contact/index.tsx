bf5af36cfbfd429ac176041c76483d54b996a02e
NurazizCoder <nuralievnuraziz0@gmail.com>
2026-09-09 16:00:46 +0500

    Workflow file added

--- src/routes/(app)/contact/index.tsx
+++ src/routes/(app)/contact/index.tsx
@@ -1,5 +1,5 @@
 import {createFileRoute} from '@tanstack/react-router'
-import {lazy} from "react";
+import React, {lazy} from "react";
 import ContactInfoRow from "@/routes/(app)/contact/-components/ContactInfoRow";
 
 
